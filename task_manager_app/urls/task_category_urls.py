5f3c8df9369067550fae4d3bc01a69d73d76762e
Abd-Alsalam-Jodalah <129756251+Abd-Alsalam-Jodalah@users.noreply.github.com>
2025-02-20 00:20:26 +0200

    Fix urls, and add category urls

--- task_manager_app/urls/task_category_urls.py
+++ task_manager_app/urls/task_category_urls.py
@@ -3,5 +3,5 @@ from ..controllers.task_category_controllers import TaskCategoryApi
 
 urlpatterns = [
     path('category/', TaskCategoryApi.as_view(), name='category-crud'),
-    path('categories/<int:pk>/', TaskCategoryApi.as_view(), name='category-detail'), 
+    path('category/<int:pk>/', TaskCategoryApi.as_view(), name='category-detail'), 
       ]
--- task_manager_app/urls/task_urls.py
+++ task_manager_app/urls/task_urls.py
@@ -4,7 +4,8 @@ from ..controllers.task_controllers import TaskApi, ByUser
 urlpatterns = [
     path('task/',           TaskApi.as_view(), name='task-crud'),
     path('task/<uuid:pk>/', TaskApi.as_view(), name='task-id-crud'),
-    path('task/byuser/',        ByUser.as_view(), name='task-id-crud'),
-    path('task/',        ByUser.as_view(), name='task-id-crud'),
+    
+    path('task/byuser/',            ByUser.as_view(), name='task-byuser-crud'),
+    path('task/byuser/<uuid:pk>',   ByUser.as_view(), name='task-byuser-id-crud'),
     
 ]
